7c9315f1256a45dce069ffd4b2595d5d944220aa
Santiago Barrios <santibarrios412@gmail.com>
2023-02-09 12:49:54 -0500

    The code from API-Backend

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+//import packages - updated
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	//example for now, but should invoke scraper.go to get cat objects.
+	cats = append(cats, Cat{Name: "Smithy", Age: "1 year old", Gender: "Male", Loc: "Gainesville Cat Cafe", ID: "A245676"})
+	cats = append(cats, Cat{Name: "Ruby", Age: " 4 months old", Gender: "Female", Loc: "Miami Animal Shelter", ID: "A674232"})
+
+	router.HandleFunc("/cats", getCats).Methods("Get")           //read all cats
+	router.HandleFunc("/cats/{id}", getCat).Methods("GET")       // read one cat
+	router.HandleFunc("/cats/{id}", updateCat).Methods("PUT")    // update cat info
+	router.HandleFunc("/cats/{id}", deleteCat).Methods("DELETE") // delete cat
+	router.HandleFunc("/cats", createCat).Methods("POST")        //create a new cat
+
+	fmt.Println("Starting server at port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+
+}
