385b5777a024b43a6538d1c19b3b8b338a461bc9
Santiago Barrios <santibarrios412@gmail.com>
2023-02-27 22:28:32 -0500

    added test for getCats

--- api_test.go
+++ api_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func testgetCats(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/cats", nil)
+
+	//error checking
+	if err != nil {
+
+		t.Fatal(err)
+	}
+
+	responseRecorder := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/cats", getCats)
+
+	router.ServeHTTP(responseRecorder, req)
+
+}
