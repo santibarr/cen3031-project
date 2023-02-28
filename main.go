209cb1bbbb10e9a8068ab62750edc2e99ec73419
Santiago Barrios <santibarrios412@gmail.com>
2023-02-27 22:14:23 -0500

    Backend-Frontend successful, working on tests

--- main.go
+++ main.go
@@ -14,15 +14,15 @@ func main() {
 	//router created
 	router := mux.NewRouter()
 
-	//this should work
-	fs := http.FileServer(http.Dir("src"))
+	//Err would be 404 not found, Directory does not exist
+	fs := http.FileServer(http.Dir("dist/purfect-partner"))
 	router.PathPrefix("/").Handler(http.StripPrefix("/", fs))
 
 	//example for now, but should invoke scraper.go to get cat objects.
 	cats = append(cats, Cat{Name: "Smithy", Age: "1 year old", Gender: "Male", Loc: "Gainesville Cat Cafe", ID: "A245676"})
 	cats = append(cats, Cat{Name: "Ruby", Age: " 4 months old", Gender: "Female", Loc: "Miami Animal Shelter", ID: "A674232"})
 
-	router.HandleFunc("/cats", getCats).Methods("Get")           //read all cats
+	router.HandleFunc("/cats", getCats).Methods("GET")           //read all cats
 	router.HandleFunc("/cats/{id}", getCat).Methods("GET")       // read one cat
 	router.HandleFunc("/cats/{id}", updateCat).Methods("PUT")    // update cat info
 	router.HandleFunc("/cats/{id}", deleteCat).Methods("DELETE") // delete cat
