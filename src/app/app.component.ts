f02872fb7e4d497a3b752097231fc14062aef169
Santiago Barrios <santibarrios412@gmail.com>
2023-03-29 16:04:36 -0400

    Tried to see if the ngOnInit would work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'front-end';
+  cats: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    //Make an HTTP GET request to the API
+    this.http.get('http://localhost:8080/cats').subscribe(data => {
+      //Read the result field from the JSON response.
+      this.cats = data;
+      console.log(data)
+    })
+
+  }
 }
