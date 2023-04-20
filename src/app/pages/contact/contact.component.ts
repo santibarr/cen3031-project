1fedaf014f5046823cae23c842fd53a0ddc0f94b
santibarr <92824641+santibarr@users.noreply.github.com>
2023-04-20 00:12:27 -0400

    new changes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,8 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  constructor() { }
+  firstName: string | undefined;
+  lastName: string | undefined;
+  email: string | undefined;
+  phone: string | undefined;
+  
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
+    //print debug
+    //console.log("created");
+  }
+
+  onFormSubmit(event: Event) {
     
+    //send the user data to the server
+    const User = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      phone: this.phone
+    };
+
+    //print debug
+    //console.log("helloooooo")
+
+    this.http.post('http://localhost:8000/contact', User).subscribe(data => {
+      console.log(data);
+      
+    });
   }
 }
